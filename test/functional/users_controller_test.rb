84ffdbeb674e242813b2469eebb9f60468bddd76
conzett <conzett@gmail.com>
2012-11-12 12:06:17 -0500

    Remove create action from users controller
    This is handled in the registrations controller. Might combine these at a later time if possible, right now devise like to handle the controllers this way.

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,26 +2,13 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   setup do
-    @user = users(:one)
+    @user = create(:user)
   end
 
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create user" do
-    assert_difference('User.count') do
-      post :create, user: {  }
-    end
-
-    assert_redirected_to user_path(assigns(:user))
   end
 
   test "should show user" do
