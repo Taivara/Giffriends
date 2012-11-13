d655103de1870576424d04881e647cac48fda501
conzett <conzett@gmail.com>
2012-11-13 15:50:46 -0500

    Clean up authentications controller

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,20 +3,20 @@ class AuthenticationsController < ApplicationController
   def create
     omniauth = request.env["omniauth.auth"]
     authentication = Authentication.find_by_provider_and_uid(
-      omniauth['provider'], omniauth['uid'])
+      omniauth[:provider], omniauth[:uid])
     if authentication
-      flash[:notice] = "Signed in successfully."
+      flash[:notice] = t(:signed_in_successfully)
       sign_in_and_redirect(:user, authentication.user)
     elsif current_user
       current_user.authentications.create(
-        provider: omniauth['provider'], uid: omniauth['uid'])
-      flash[:notice] = "Authentication successful."
+        provider: omniauth[:provider], uid: omniauth[:uid])
+      flash[:notice] = t(:authentication_successfull)
       redirect_to edit_user_registration_url
     else
       user = User.new
       user.apply_omniauth(omniauth)
       if user.save
-        flash[:notice] = "Signed in successfully."
+        flash[:notice] = t(:signed_in_successfully)
         sign_in_and_redirect(:user, user)
       else
         session[:omniauth] = omniauth.except('extra')
@@ -28,7 +28,7 @@ class AuthenticationsController < ApplicationController
   def destroy
     @authentication = current_user.authentications.find(params[:id])
     @authentication.destroy
-    flash[:notice] = "Successfully destroyed authentication."
+    flash[:notice] = t(:successfully_removed_authentication)
     redirect_to edit_user_registration_url
   end
 end
