b2f55bae8e8085c6bb8a441fbfc2d1a93c72884d
Zach <zmcartor@gmail.com>
2013-03-03 17:56:39 -0500

    Added up/down methods to migration

--- db/migrate/20130302193303_additional_fields_to_user.rb
+++ db/migrate/20130302193303_additional_fields_to_user.rb
@@ -1,8 +1,14 @@
 class AdditionalFieldsToUser < ActiveRecord::Migration
 
+def up
   change_table :users do |t|
     t.integer :age
     t.date :birthday
   end
+end
 
+def down
+  remove_column :users, :age
+  remove_column :users, :birthday
+end
 end
