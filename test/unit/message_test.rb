9ece0985460f9d70f633985b510143ae0cfec5e6
Zach <zmcartor@gmail.com>
2013-03-02 19:48:59 -0500

    Message relation test

--- test/unit/message_test.rb
+++ test/unit/message_test.rb
@@ -10,4 +10,17 @@ class MessageTest < ActiveSupport::TestCase
     assert !mess.save , 'message saved with no text, bummer!'
   end
 
+  test 'message has a receiver and sender' do
+    message = build(:message)
+    sender = create(:user , email: 'kimjongun@dprk.kr')
+    receiver = create(:user , email: 'barak@us.gov')
+
+    message.to = receiver.id
+    message.from = sender.id
+    message.save
+
+    assert receiver.messages_inbox.count ==1, 'message not in receiver inbox!'
+    assert sender.messages_outbox.count ==1, 'message not in sender outbox!'
+  end
+
 end
